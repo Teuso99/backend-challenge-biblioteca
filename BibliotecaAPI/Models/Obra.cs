5bbfdd3d9b7c8bc62c5fa6ced0ccf515266e6578
Mateus Machado <mateusmmachado2@gmail.com>
2021-04-18 14:20:10 -0300

    adiciona Model da obra e DB local

---
+++ BibliotecaAPI/Models/Obra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaAPI.Models
+{
+    public class Obra
+    {
+        public long Id { get; set; }
+        public string Titulo { get; set; }
+        public string Editora { get; set; }
+        public string Foto { get; set; }
+        public string Autores { get; set; }
+    }
+}
---
+++ BibliotecaAPI/Models/ObraContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaAPI.Models
+{
+    public class ObraContext : DbContext
+    {
+        public ObraContext(DbContextOptions<ObraContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Obra> Obra { get; set; }
+    }
+}
